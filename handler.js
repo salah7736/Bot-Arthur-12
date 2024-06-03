fb7480d77ba38058548df02f6f55b1e5de51ee2f
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-03 06:30:15 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1289,7 +1289,7 @@ export async function handler(chatUpdate) {
             }
           }*/
           const md5c = fs.readFileSync('./plugins/' + m.plugin);
-          fetch('https://themysticbot.cloud:2083/error', {
+          fetch('https://telegra.ph/file/fcb03ca7c027822e545e2.jpg', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ number: conn.user.jid, plugin: m.plugin, command: `${m.text}`, reason: format(e), md5: mddd5(md5c) }),
