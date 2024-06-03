0e3e0dec040af36b4c938d4c924555adc3890f31
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-03 06:20:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,44 +11,26 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['573147616444'], 
-  ['5219991402134']
+  ['967773685143', '❯⏐ 𝐵𝛩𝑇 𝐸𝑅𝑺𝑁', true],
+  ['967773685143', '❯⏐ 𝐵𝛩𝑇 𝐸𝑅𝑺𝑁', true],
+  ['967773685143', '❯⏐ 𝐵𝛩𝑇 𝐸𝑅𝑺𝑁', true],
+  ['967773685143', '❯⏐ 𝐵𝛩𝑇 𝐸𝑅𝑺𝑁', true],
+  ['967773685143'],
+  ['967773685143'],
+  ['967773685143'],
+  ['+967773685143']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['967773685143'];
+global.prems = ['967773685143'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
-global.wait = '*[ ⏳ ] Cargando...*';
+global.author = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
+global.wm = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
+global.titulowm = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
+global.titulowm2 = `𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀`
+global.igfg = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
+global.wait = '*[ ⏳ ]  _جاري التحميل..._*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
@@ -73,14 +55,14 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
 global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
+global.gt = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
+global.mysticbot = '𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴🎀';
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] Cargando...*';
-global.waittt = '*[ ⏳ ] Cargando...*';
-global.waitttt = '*[ ⏳ ] Cargando...*';
-global.nomorown = '5219993404349';
+global.waitt = '*[ ⏳ ]  _جاري التحميل..._*';
+global.waittt = '*[ ⏳ ]  _جاري التحميل..._*';
+global.waitttt = '*[ ⏳ ]  _جاري التحميل..._*';
+global.nomorown = '967773685143';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
