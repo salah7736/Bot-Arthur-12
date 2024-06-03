6b6173451033a1b32c97f8895052dacdef66b75d
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-03 22:32:57 +0300

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -68,7 +68,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = ['mp3', 'songs', 'اغنيه']
+handler.command = ['اغاني', 'غنيه', 'غنية']
 
 handler.exp = 0;
 handler.diamond = false;
