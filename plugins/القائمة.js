de47e1c2dd097f5c59b4f31e509fac742eb689af
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-07 01:17:18 +0300

    Update القائمة.js

--- plugins/القائمة.js
+++ plugins/القائمة.js
@@ -14,72 +14,23 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                 {
                   name: 'single_select',
                   buttonParamsJson: JSON.stringify({
-                   title: 'دوس عليا 💔',
-                   title: '.المطور',
+                    title: 'دوس هنا ',
                     sections: [
                       {
-                        title: 'List',
-                        highlight_label: 'بوت',
+                        title: 'قوائم البوت',
+                        highlight_label: 'اختار',
                         rows: [
-                            {
-                            header: '*❯⏐اوامـر الـجـروبـات↞📜*',
-                            title: 'ميزو1',
+                          {
+                            header: 'قسم الانمي',
+                            title: '.المطور',
                             description: '',
-                            id: '.ميزو1'
-                          },
-                            {
-                            header: '*❯⏐اوامـر الأعـضـاء↞🪽*',
-                            title: '.ميزو2',
-                            description: '',
-                            id: '.ميزو2'
-                          },
-                            {
-                            header: '*❯⏐اوامـر  الـترفـيـه↞🎀*',
-                            title: '.ميزو3',
-                            description: '',
-                            id: '.ميزو3'
-                          },
-                            {
-                            header: '*❯⏐اوامـر  التـحمـيل↞🖨️*',
-                            title: '.ميزو4',
-                            description: '',
-                            id: '.ميزو4'
-                          },
-                            {
-                            header: '*❯⏐اوامـر الـصـور↞☘️*',
-                            title: '.ميزو5',
-                            description: '',
-                            id: '.ميزو5'
-                          },
-                            {
-                            header: '*❯⏐اوامـر الـلـفـل↞🏅*',
-                            title: '.ميزو6',
-                            description: '',
-                            id: '.ميزو6'
-                          },
-                            {
-                            header: '*❯⏐اوامـر الملصقات↞❄️*',
-                            title: '.ميزو7',
-                            description: '',
-                            id: '.ميزو7'
-                          },
-                            {
-                            header: '*❯⏐اوامـر ديـنـيـه↞🕌*',
-                            title: '.ميزو8',
-                            description: '',
-                            id: '.ميزو8'
+                            id: 'te'
                           },
                           {
-                            header: '*❯⏐اوامـر الـمطـور↞👑*',
-                            title: 'بوت',
+                            header: 'قسم الاوامر',
+                            title: '.الاوامر',
                             description: '',
-                            id: 'بوت'
-                          },
-                          {
-                            header: '*❯⏐جـمـيـع الاوامـر ↞💎*',
-                            title: '.ميزو10',
-                            description: '',
-                            id: '.ميزو10'
+                            id: 'te'
                           }
                         ]
                       }
@@ -98,6 +49,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['info']
 handler.tags = ['main']
-handler.command = ['القائمة']
+handler.command = ['قايمه']
 
 export default handler
