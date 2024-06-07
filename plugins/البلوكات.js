069d2eb670275a0463a5c138ee6e05ee747f9369
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-08 02:41:59 +0300

    Update البلوكات.js

--- plugins/البلوكات.js
+++ plugins/البلوكات.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 await conn.fetchBlocklist().then(async data => {
 let txt = `*≡ قائمة المحظورين من البوت*\n\n*المجموع :* ${data.length}\n\n┌─⊷\n`
 for (let i of data) {
-txt += `▢ @${i.split("@")[0]}\n`}
+txt += `〄┇ @${i.split("@")[0]}\n`}
 txt += "└───────────"
 return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 }).catch(err => {
