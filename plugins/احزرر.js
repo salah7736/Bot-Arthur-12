5062370fbe72b4d16e8b17c0990afc633186cb7d
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-07 15:22:30 +0300

    Update احزرر.js

--- plugins/احزرر.js
+++ plugins/احزرر.js
@@ -4,22 +4,25 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '⚡️┃لم يتم الاجابة علي السؤال بعد┃⚡️ ❯', conn.tebakbendera[id][0])
+        conn.reply(m.chat, '*❮⚡┃لم يتم الاجابة علي السؤال بعد┃⚡️❯*', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/98d564d49cbf9b539fee19f744de7b26/raw/f2a3e68bbcdd2b06f9dbd5f30d70b9fda42fec14/guessflag')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}*
-  ⎔↞┃الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)} ┃
-  *استخدم .انسحب للأنسحاب*
-  ⎔↞┃الـجـائـزة🎖↞ ${poin} نقاط┃
-┃𝐵𝛩𝑇-𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴┃↞⎔
+  let caption = `*˼‏❖˹┇⇠『${command.toUpperCase()}』*
+*「✧|────✦❯◇❮✦────|✧」*
+  *〄↞┇الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)}┇
+  *〄↞┇الـجـائـزة🎖↞ ${poin}┇
+*「✧|────✦❯◇❮✦────|✧」*
+> استخدم .انسحب للأنسحاب‼️
+*✧━ • ━ 「 ✤ 」 ━ • ━✧*
+> ❯⏐ 𝐵𝛩𝑇 𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ 💧┇انتهي الوقت┇💧❯\n❐↞┇الاجـابـة✓↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ 💧┇انتهي الوقت┇💧❯*\n*❐↞┇الاجـابـة✓↞ ${json.name}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
