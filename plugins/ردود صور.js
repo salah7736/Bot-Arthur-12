d1ccb06476abbaf961a0e4ac587cab38ebfec1be
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-08 23:28:52 +0300

    Update ردود صور.js

--- plugins/ردود صور.js
+++ plugins/ردود صور.js
@@ -11,3 +11,18 @@ handler.customPrefix = /^(ايريه)$/i;
 handler.command = new RegExp;
 
 export default handler;
+
+___________________
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `${taguser}\n كم مره اقلك اذلف`;
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/280978fedc8d6d44250c2.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(ايم)$/i;
+handler.command = new RegExp;
+
+export default handler;
