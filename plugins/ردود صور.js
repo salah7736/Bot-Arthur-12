ff503189acaebeb27e9506c0547dcfe9ce8a1b6f
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-08 22:56:47 +0300

    Update ردود صور.js

--- plugins/ردود صور.js
+++ plugins/ردود صور.js
@@ -2,55 +2,12 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `${taguser}\n*✿┇اهـلًا مـعـك بـوت مـيـزوهـارا للمـساعـدة اكــتب الـاوامـر او تـضيف البـوت اكتـب المـطور ┇✿*`;
+    let message = `${taguser}\n كم مره اقلك اذلف`;
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/fcb03ca7c027822e545e2.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/280978fedc8d6d44250c2.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(ايريين|ارثرر)$/i;
-handler.command = new RegExp;
-
-export default handler;
-///
-let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `${taguser}\n*✿┇اهـلًا مـعـك بـوت مـيـزوهـارا للمـساعـدة اكــتب الـاوامـر او تـضيف البـوت اكتـب المـطور ┇✿*`;
-
-    conn.sendFile(m.chat, 'https://telegra.ph/file/fcb03ca7c027822e545e2.jpg', 'image.jpg', message, m);
-};
-
-handler.customPrefix = /^(ميي)$/i;
-handler.command = new RegExp;
-
-export default handler;
-
-//
-let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `${taguser}\n*✿┇اهـلًا مـعـك بـوت مـيـزوهـارا للمـساعـدة اكــتب الـاوامـر او تـضيف البـوت اكتـب المـطور ┇✿*`;
-
-    conn.sendFile(m.chat, 'https://telegra.ph/file/fcb03ca7c027822e545e2.jpg', 'image.jpg', message, m);
-};
-
-handler.customPrefix = /^(سات|ساات|ساتا)$/i;
-handler.command = new RegExp;
-
-export default handler;
-//
-let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `${taguser}\n*✿┇اهـلًا مـعـك بـوت مـيـزوهـارا للمـساعـدة اكــتب الـاوامـر او تـضيف البـوت اكتـب المـطور ┇✿*`;
-
-    conn.sendFile(m.chat, 'https://telegra.ph/file/fcb03ca7c027822e545e2.jpg', 'image.jpg', message, m);
-};
-
-handler.customPrefix = /^(بوتا|مينا)$/i;
+handler.customPrefix = /^(ايريه)$/i;
 handler.command = new RegExp;
 
 export default handler;
