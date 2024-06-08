770aa8517456d4af8d5647b499257a40ed034ad3
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-08 15:47:11 +0300

    Update رياضيات.js

--- plugins/رياضيات.js
+++ plugins/رياضيات.js
@@ -1,30 +1,32 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.math = conn.math ? conn.math : {}
     
-    if (args.length < 1) throw `
-  🧮 الصعوبات المتاحة : 
+   if (args.length < 1) throw `
+  「✧|────✦❯◇❮✦────|✧」
+  〖🧮 الصعوبات المتاحة :〗 
   
 ${Object.keys(modes).join(' | ')} 
 
-_📌مثال : ${usedPrefix+command} عادي_
+˼‏❖˹┇⇠『_📌مثال : ${usedPrefix+command} عادي_』
 `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
-  🧮 الصعوبات المتاحة : 
+ 「✧|────✦❯◇❮✦────|✧」
+  〖🧮 الصعوبات المتاحة :〗 
   
  ${Object.keys(modes).join(' | ')}
 
-_📌مثال : ${usedPrefix+command} عادي_
+˼‏❖˹┇⇠『_📌مثال : ${usedPrefix+command} عادي_』
 `.trim()
     
   let id = m.chat
     if (id in conn.math) return conn.reply(m.chat, '*┃⚠️ لا تزال هناك أسئلة دون إجابة في هذه الدردشة┃*', conn.math[id][0])
     let math = genMath(mode)
     conn.math[id] = [
-        await conn.reply(m.chat, `احسب${math.str}*=\n\n_الوقت:_ ${(math.time / 1000).toFixed(2)} ثواني\n\n🎁 جائزة : ${math.bonus} XP`, m),
+        await conn.reply(m.chat, `✧━━ • ━ 「 ✤ 」 ━ • ━━✧\n〄↞┇احسب${math.str}*=\n\n〄↞┇_الوقت:_ ${(math.time / 1000).toFixed(2)} ثواني┇\n\n〄↞┇🎁 جائزة : ${math.bonus} XP┇\n✧━━ • ━ 「 ✤ 」 ━ • ━━✧`, m),
         math, 4,
         setTimeout(() => {
-            if (conn.math[id]) conn.reply(m.chat, `⏳ أنتهى الوقت!\nالإجابة هي : *${math.result}*`, conn.math[id][0])
+            if (conn.math[id]) conn.reply(m.chat, `〖⏳ أنتهى الوقت!〗\n\n〄↞┇الإجابة هي : *${math.result}┇*`, conn.math[id][0])
       delete conn.math[id]
         }, math.time)
     ]
