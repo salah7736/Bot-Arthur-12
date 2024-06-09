9b422c651f9ee037b8305927c4a0e3c7c7dc80fa
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-09 19:59:42 +0300

    Update رد-ايرين.js

--- plugins/رد-ايرين.js
+++ plugins/رد-ايرين.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'https://telegra.ph/file/ae2a5c0e2fdd0db349433.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(ايرين|ايريه)$/i;
+handler.customPrefix = /^(ايرين)$/i;
 handler.command = new RegExp;
 
 export default handler;
