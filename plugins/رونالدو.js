9ebcd4c7a925af04043bae757c02c0a8f8aeea43
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-10 14:28:58 +0300

    Add files via upload

--- plugins/رونالدو.js
+++ plugins/رونالدو.js
@@ -4,14 +4,6 @@ let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-},
-{
-      name: 'quick_reply',
-     buttonParamsJson: JSON.stringify({
-    display_text: 'التالي',
-    id: `.رونالدو`
-  })
-},
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['img']
 handler.command = /^(رونالدو|cr7)$/i
