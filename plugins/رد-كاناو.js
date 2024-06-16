3a2e94687b2695853cfe82e8e5de5c53db7d5145
salah7736 <yemen777eren@gmail.com>
2024-06-16 08:02:46 +0300

    Add files via upload

--- plugins/رد-كاناو.js
+++ plugins/رد-كاناو.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*${taguser}*\n*مـزة كيميتسو❄️*`;
+    let message = `*${taguser}*\n* عمتك و مـزة كيميتسو❄️*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/03bef07dfae8c091fec04.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(كاناات)$/i;
+handler.customPrefix = /^(كاناو)$/i;
 handler.command = new RegExp;
 
 export default handler;
