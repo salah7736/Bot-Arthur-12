6418a3b7c04d1a7b3a21bda80c7c8fc7253afdd0
salah7736 <yemen777eren@gmail.com>
2024-06-16 09:00:26 +0300

    Update رد-تيسيا.js

--- plugins/رد-تيسيا.js
+++ plugins/رد-تيسيا.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*${taguser}*\n *وش تـبـي مـن عـمـتك🫠🌸*`;
+    let message = `*${taguser}*\n *「وش تـبـي مـن عـمـتك🫠🌸」*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/d5d676261a0445d87fdb0.jpg', 'image.jpg', message, m);
 };
