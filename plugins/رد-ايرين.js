8402e1518fbf8d8c9b3f42bb712f31426844c7f1
salah7736 <yemen777eren@gmail.com>
2024-06-16 09:02:06 +0300

    Update رد-ايرين.js

--- plugins/رد-ايرين.js
+++ plugins/رد-ايرين.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*${taguser}*\n *اعـظـم بـطـل فـي الـعـالـم✨🖤*`;
+    let message = `*${taguser}*\n *「اعـظـم بـطـل فـي الـعـالـم✨🖤」*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/ae2a5c0e2fdd0db349433.jpg', 'image.jpg', message, m);
 };
