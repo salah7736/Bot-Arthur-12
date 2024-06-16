8f8463dded2dda78825e0f052b8b0edc300e7312
salah7736 <yemen777eren@gmail.com>
2024-06-16 08:59:43 +0300

    Update رد-ارثر.js

--- plugins/رد-ارثر.js
+++ plugins/رد-ارثر.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*${taguser}*\n *يـاورع خـلاص تـعبـت وانـا ارد 🙄*`;
+    let message = `*${taguser}*\n *「يـاورع خـلاص تـعبـت وانـا ارد 🙄」*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/c55c0eb6afb55c3dc5077.jpg', 'image.jpg', message, m);
 };
