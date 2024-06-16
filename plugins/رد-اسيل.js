1988e2bb24fb02384eed5a0b1c3326a2c6ecfb05
salah7736 <yemen777eren@gmail.com>
2024-06-16 09:01:14 +0300

    Update رد-اسيل.js

--- plugins/رد-اسيل.js
+++ plugins/رد-اسيل.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*${taguser}*\n *سأحتل زيكولا من أجل أسيل🪽*`;
+    let message = `*${taguser}*\n *「سأحتل زيكولا من أجل أسيل🪽」*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/92a2d035ee8ac62adddf7.jpg', 'image.jpg', message, m);
 };
