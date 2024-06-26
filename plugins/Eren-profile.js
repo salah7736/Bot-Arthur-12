d810beaccfae1dec3a5d888831e7802eb0ca4eaf
salah7736 <yemen777eren@gmail.com>
2024-06-26 12:28:18 +0300

    Update Eren-profile.js

--- plugins/Eren-profile.js
+++ plugins/Eren-profile.js
@@ -14,7 +14,8 @@ let handler = async (m, { conn }) => {
 *⎔↞┃منشن☘️ ↞* @${who.replace(/@.+/, '')}
 *⎔↞┃لفلك📮 ↞* ${level}
 *⎔↞┃رابط⚡️ ↞* https://wa.me/${who.split`@`[0]}
-*⎔↞┃عمر🔍↞* ${registered ? age : ''} Tahun
+*⎔↞┃انذارات🔍↞* ${warn}/${maxwarn}
+
 *⎔↞┃شريك🍁 ↞*  ${pasangan ? `@${pasangan.split("@")[0]}` : `Tidak Punya`}
 *⎔↞┃مسجل📠 ↞* ${registered ? 'نعم': 'لا'}
 *⎔↞┃مميز🌷 ↞* ${premium ? "نعم" :"لا"}
@@ -39,52 +40,3 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, '*Hari*\n ', h, ' *Jam*\n ', m, ' *Menit*\n ', s, ' *Detik* '].map(v => v.toString().padStart(2, 0)).join('')
 }
-///
-import { createHash } from 'crypto'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-  let who = m.quoted
-    ? m.quoted.sender
-    : m.mentionedJid && m.mentionedJid[0]
-      ? m.mentionedJid[0]
-      : m.fromMe
-        ? conn.user.jid
-        : m.sender
-  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-  let user = global.db.data.users[who]
-  let about = ((await conn.fetchStatus(who).catch(console.error)) || {}).status || ''
-  let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } =
-    global.db.data.users[who]
-  let { min, xp, max } = xpRange(user.level, global.multiplier)
-  let username = conn.getName(who)
-  let math = max - xp
-  let prem = global.prems.includes(who.split`@`[0])
-  let sn = createHash('md5').update(who).digest('hex')
-
-  // • @${who.replace(/@.+/, '')}
-  let str = `*🪪 Name:* ${username}${about ? '\n\n 🎌 *Bio:* ' + about : ''}
-
-*⚠️ Warnings:* ${warn}/${maxwarn}
-
-*💰 Gold :* ${credit}
-
-*✨ Level* : ${level}
-
-*⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `Ready for *${usedPrefix}levelup*` : `*${math}xp* missing to level up`}
-
-*🏆 Rank:* ${role}
-
-*📇 Registered :* ${registered ? 'Yes' : 'No'}
-
-*⭐ Premium* : ${prem ? 'Yes' : 'No'}
-`
-  conn.sendFile(m.chat, pp, 'profil.jpg', str, m, false, { mentions: [who] })
-  m.react(done)
-}
-handler.help = ['profile']
-handler.tags = ['group']
-handler.command = ['profile']
-
-export default handler
